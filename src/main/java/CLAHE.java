7c84ff81ce1543e2f4b342753827b5b32310e662
Sacha Maire <sacha.maire18@imperial.ac.uk>
2020-11-27 17:05:56 +0100

    Change project definition to use openCV and write code to apply it

--- src/main/java/CLAHE.java
+++ src/main/java/CLAHE.java
@@ -1,16 +1,27 @@
-import com.sun.org.apache.xml.internal.security.algorithms.Algorithm;
-import org.opencv.imgproc;
+
 import org.w3c.dom.Document;
+import org.opencv.core.Algorithm;
+import org.opencv.core.Imgproc;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CLAHE extends Algorithm {
 
 
-    public CLAHE(Document doc, String algorithmURI) {
-        super(doc, algorithmURI);
-    }
-
-    @Override
-    public String getBaseLocalName() {
-        return null;
+    private static Mat[] applyCLAHE(Mat img, Mat L) {
+        Mat[] result = new Mat[2];
+        CLAHE clahe = Imgproc.createCLAHE();
+        clahe.setClipLimit(2.0);
+        Mat L2 = new Mat();
+        clahe.apply(L, L2);
+        Mat LabIm2 = new Mat();
+        List<Mat> lab = new ArrayList<>();
+        Core.split(img, lab);
+        Core.merge(new ArrayList<>(Arrays.asList(L2, lab.get(1), lab.get(2))), LabIm2);
+        Mat img2 = new Mat();
+        Imgproc.cvtColor(LabIm2, img2, Imgproc.COLOR_Lab2BGR);
+        result[0] = img2;
+        result[1] = L2;
+        return result;
     }
 }
