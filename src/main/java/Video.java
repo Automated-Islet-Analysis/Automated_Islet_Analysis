e269b1d968e883c14464dab541efecb59067a17a
gaspardol <oliviers.gaspard@gmail.com>
2020-12-03 09:43:46 +0100

    Implementation of motion correction and video class
    First implementation of motion correction, needs to be combined with first GUI to have first version of our product

---
+++ src/main/java/Video.java
@@ -0,0 +1,109 @@
+import ImageJ.Stack_Splitter;
+import ImageJ.nifti_io.Nifti_Reader;
+import ij.ImagePlus;
+import ij.io.FileSaver;
+import ij.io.Opener;
+import ij.plugin.FolderOpener;
+import org.itk.simple.Image;
+import org.itk.simple.SimpleITK;
+import org.opencv.core.Core;
+import java.io.File;
+import java.util.LinkedList;
+
+
+public class Video {
+    static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+    protected String filename;
+    protected LinkedList<Image> frames = new LinkedList();
+    protected LinkedList<Image> alignedFrames = new LinkedList();
+    protected int numberOfFrames=0;
+    protected String name;
+    protected String dirName;
+
+    // Constructors
+    public Video(){}
+
+    public Video(String filename){
+        this.filename=filename;
+        File file = new File(filename);
+        name = file.getName();
+        dirName = file.getParent();
+
+        readFrames();
+    }
+
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public void setAlignedFrames(LinkedList<Image> alignedFrames) {
+        this.alignedFrames = alignedFrames;
+    }
+
+    public LinkedList<Image> getAlignedFrames() {
+        return alignedFrames;
+    }
+
+    private void readFrames() {
+        ImagePlus vid = new ImagePlus();
+        Opener opener = new Opener();
+
+        vid = opener.openImage(filename);
+        numberOfFrames = vid.getStackSize();
+        Stack_Splitter stack_splitter = new Stack_Splitter();
+        stack_splitter.run(vid);
+
+        for (int i = 1; i <= numberOfFrames; i++) {
+            Image img1 = new Image();
+            img1 = SimpleITK.readImage(System.getProperty("user.dir") + "/temp/img/" + String.valueOf(i) + ".tif");
+            frames.add(img1);
+        }
+        File directory = new File(System.getProperty("user.dir") + "/temp/img");
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if (!file.delete()) {
+                System.out.println("Failed to delete " + file);
+            }
+        }
+
+    }
+
+    public void saveAlignedFrames() {
+        for (int i = 1; i < numberOfFrames - 1; i++) {
+
+            // Save as nii format used by SimpleElastix
+            SimpleITK.writeImage(alignedFrames.get(i), "temp/temp.nii");
+
+            ImagePlus img = new ImagePlus();
+            File file = new File(System.getProperty("user.dir") + "/temp/temp.nii");
+            Nifti_Reader nifti_reader = new Nifti_Reader();
+            img = nifti_reader.run(file);
+
+            FileSaver fileSaver1 = new FileSaver(img);
+            fileSaver1.saveAsPng(System.getProperty("user.dir") + "/temp/img/aligned" + String.valueOf(i) + ".png");
+
+        }
+
+        // Combine all the .png into a stack to save it as a video(tiff format)
+        ImagePlus alignedVid = new ImagePlus();
+        FolderOpener folderOpener = new FolderOpener();
+        alignedVid = folderOpener.open(System.getProperty("user.dir") + "/temp/img");
+
+        FileSaver fileSaver2 = new FileSaver(alignedVid);
+        fileSaver2.saveAsTiff(System.getProperty("user.dir") + "/videos/" + name.lastIndexOf(".") + "_aligned_vid.tif");
+
+        // Remove all temporary files from img
+        File directory = new File(System.getProperty("user.dir")+"/temp/img");
+        File[] files = directory.listFiles();
+        for (File file : files){
+            if (!file.delete()){System.out.println("Failed to delete "+file);}
+        }
+    }
+}
+
+
---
+++ src/main/java/VideoProcessor.java
@@ -0,0 +1,73 @@
+import org.itk.simple.ElastixImageFilter;
+import org.itk.simple.Image;
+import org.itk.simple.ParameterMap;
+import org.itk.simple.SimpleITK;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.CLAHE;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class VideoProcessor extends Video{
+    public VideoProcessor(Video video){
+        this.filename=video.filename;
+        this.name = video.name;
+        this.dirName = video.dirName;
+        this.frames = video.frames;
+        this.alignedFrames = video.alignedFrames;
+        this.numberOfFrames= video.numberOfFrames;
+    }
+
+    // Perform motion correction on frames
+    public void motionCorrect(){
+        if (numberOfFrames<1){
+            System.out.println("Error: motion correction on 0 frames");
+        }
+        else {
+
+            ParameterMap parMap = SimpleITK.getDefaultParameterMap("translation");
+            //parMap["ResultImageFormat"] = ['tif'];
+
+            ElastixImageFilter fil = new ElastixImageFilter();
+            fil.setFixedImage(frames.get(0));
+            fil.setParameterMap(parMap);
+            fil.printParameterMap();
+
+            Image img = new Image();
+            String msg;
+            msg = img.getPixelIDTypeAsString();
+            System.out.println(msg);
+
+            for (int i=1; i<numberOfFrames ; i++){ /***/
+                fil.setMovingImage(frames.get(i));
+                fil.execute();
+
+                Image out = new Image();
+                out = fil.getResultImage();
+                alignedFrames.add(out);
+
+            }
+        }
+    }
+
+    public static Mat[] applyCLAHE(Mat img, Mat L) {
+        Mat[] result = new Mat[2];
+        CLAHE clahe = Imgproc.createCLAHE();
+        clahe.setClipLimit(2.0);
+        Mat L2 = new Mat();
+        clahe.apply(L, L2);
+        Mat LabIm2 = new Mat();
+        List<Mat> lab = new ArrayList<>();
+        Core.split(img, lab);
+        Core.merge(new ArrayList<>(Arrays.asList(L2, lab.get(1), lab.get(2))), LabIm2);
+        Mat img2 = new Mat();
+        Imgproc.cvtColor(LabIm2, img2, Imgproc.COLOR_Lab2BGR);
+        result[0] = img2;
+        result[1] = L2;
+        return result;
+    }
+}
