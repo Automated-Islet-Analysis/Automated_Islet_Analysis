86c08cb960852240ef414540f4c3e4d1fccf5649
liv-soro <70065786+liv-soro@users.noreply.github.com>
2020-12-09 10:53:46 +0000

    intermediate home item class

---
+++ src/main/java/Home.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemListener;
+
+abstract public class Home implements ActionListener, ItemListener {
+    // upload button and welcome message
+
+    //Variable declaration
+    private javax.swing.JFrame home_frame;
+    private javax.swing.JButton jb_upload;
+    private javax.swing.JTextField jtext_welcome;
+
+    //home_frame.setSize(500,500);
+    //home_frame.setVisible(true);
+
+    public Home(){
+        init_home();
+    }
+
+    private void init_home(){
+        //jb_upload;
+    }
+}
