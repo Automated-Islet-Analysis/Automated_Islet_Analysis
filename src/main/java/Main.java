8d8313f28dc62ccf7afa656989be6e1aa25c6e55
gaspardol <oliviers.gaspard@gmail.com>
2020-12-10 16:24:53 +0100

    changes to path in unit testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import imageprocessing.VideoProcessor;
 public class Main {
     public static void main(String[] args) {
         // Init video file
-        String fileName = new String("/Users/fabio/desktop/university/YEAR_3/programming_3/Project/prg3_img/1.tif");
+        String fileName = new String(System.getProperty("user.dir")+"/videos/1.tif");
         Video vid = new Video(fileName);
 
         // Apply processing on the video
--- src/test/java/TestVideo.java
+++ src/test/java/TestVideo.java
@@ -5,34 +5,36 @@ import org.itk.simple.SimpleITK;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.awt.*;
+
 import java.util.LinkedList;
 import org.itk.simple.Image;
 
 public class TestVideo{
     @Test
     public void testReadingFrames(){
-        String filename="/Users/fabio/desktop/university/YEAR_3/programming_3/Project/prg3_img/1.tif";
+        String filename=System.getProperty("user.dir")+"/videos/1.tif";
         Video video=new Video(filename);
         String expectedName="1.tif";
-        String expectedDirName="/Users/fabio/desktop/university/YEAR_3/programming_3/Project/prg3_img";
+        String expectedDirName=System.getProperty("user.dir")+"\\videos";
 
         Assert.assertEquals(video.getFilename(),filename);
         Assert.assertEquals(video.getName(),expectedName);
+        System.out.println(video.getDirName());
         Assert.assertEquals(video.getDirName(),expectedDirName);
 
         LinkedList<Image> SEframes= new LinkedList();
         LinkedList<ImagePlus> ijFrames=new LinkedList();
-
         SEframes=video.getSEframes();
         ijFrames=video.getijframes();
+
+
 
         Opener opener=new Opener();
         for (int i=1; i<=ijFrames.size(); i++){
             Image img1=new Image();
             ImagePlus imagePlus=new ImagePlus();
-            img1 = SimpleITK.readImage( "/Users/fabio/desktop/university/YEAR_3/programming_3/Project/prg3_img/singleFrames"+ Integer.toString(i) + "a.tif");
-            imagePlus=opener.openImage("/Users/fabio/desktop/university/YEAR_3/programming_3/Project/prg3_img/singleFrames"+ Integer.toString(i) + "a.tif");
+            img1 = SimpleITK.readImage( System.getProperty("user.dir") + "/img/Unit_testing/" + String.valueOf(i) + ".tif");
+            imagePlus=opener.openImage(System.getProperty("user.dir") + "/img/Unit_testing/" + String.valueOf(i) + ".tif");
             Assert.assertEquals(SEframes.get(i),img1);
             Assert.assertEquals(ijFrames.get(i),imagePlus);
         }
