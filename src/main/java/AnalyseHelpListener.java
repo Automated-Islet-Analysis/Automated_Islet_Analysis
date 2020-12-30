57a5be6bdec68f57c4fb671f67c05f3dc6eda7ca
liv-soro <70065786+liv-soro@users.noreply.github.com>
2020-12-30 17:52:39 +0000

    Help Message for allowed error in cross-sectional area

---
+++ src/main/java/AnalyseHelpListener.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import javax.swing.JOptionPane;
+import javax.swing.JDialog;
+import javax.swing.JTextField;
+import java.beans.*; //property change stuff
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.event.*;
+
+// Ref: https://docs.oracle.com/javase/tutorial/uiswing/examples/components/DialogDemoProject/src/components/DialogDemo.java
+
+public class AnalyseHelpListener {
+    JFrame f;
+
+    AnalyseHelpListener(){
+        JFrame f = new JFrame("Help");
+        JOptionPane.showMessageDialog(f, "Helplelepeple");
+    }
+
+
+}
--- src/main/java/AnalyseListener.java
+++ src/main/java/AnalyseListener.java
@@ -2,17 +2,24 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 class AnalyseListener implements ActionListener {
     JPanel mainPanel = new JPanel(new GridLayout(3,2));
 
-    JLabel error = new JLabel("Allowed CS error (%)");
+    JLabel error = new JLabel("Allowed CS Error (%)");
     JTextField perError = new JTextField(5);
-    JCheckBox checkPlanar = new JCheckBox("Planar motion correction");
-    JCheckBox checkDepth = new JCheckBox("Depth motion correction");
+    JCheckBox checkPlanar = new JCheckBox("Planar Motion Correction");
+    JCheckBox checkDepth = new JCheckBox("Depth Motion Correction");
     JCheckBox checkROI = new JCheckBox("Find ROIs");
 
-    Object[] options = {"Ok", "Help", "Cancel"};
+    Object[] options = {"ok","help", "cancel"};
+    /*JButton ok = new JButton("Ok");
+    JButton help = new JButton("Help");
+    JButton cancel = new JButton("Cancel");
+    JButton[] options = {ok, help, cancel};*/
+
+
 
     @Override
 
@@ -23,9 +30,18 @@ class AnalyseListener implements ActionListener {
         mainPanel.add(checkROI);
         mainPanel.add(checkDepth);
 
-        // Popup.
-        JOptionPane.showOptionDialog(null, mainPanel, "Customise analysis",
+        // Popup
+         int n = JOptionPane.showOptionDialog(null, mainPanel, "Customise Analysis",
                 JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null,
                 options, null);
+
+        if (n == JOptionPane.NO_OPTION){
+            System.out.println(n);
+            JOptionPane.showMessageDialog(null, "Choose the allowed margin of difference in cross-sectional area of the islet in between frames (suggested value: 10%)");
+        }
+        // else if JOptionPane.YES_OPTION --> call function that analyses vid!!!
     }
+
+
+
 }
