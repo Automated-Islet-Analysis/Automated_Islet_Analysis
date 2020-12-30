ced5dbc06581973a73aa0b9d563438a6ba162ace
fo1318 <fabio.oliva99@outlook.it>
2020-12-30 12:30:36 +0000

    Analyse popUp testing

--- src/main/java/AnalyseListener.java
+++ src/main/java/AnalyseListener.java
@@ -4,28 +4,46 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 class AnalyseListener implements ActionListener {
-    JPanel mainPanel = new JPanel(new GridLayout(3,2));
+    JPanel mainPanel;
 
-    JLabel error = new JLabel("Allowed CS error (%)");
-    JTextField perError = new JTextField(5);
-    JCheckBox checkPlanar = new JCheckBox("Planar motion correction");
-    JCheckBox checkDepth = new JCheckBox("Depth motion correction");
-    JCheckBox checkROI = new JCheckBox("Find ROIs");
+    JLabel error;
+    JTextField perError;
+    JCheckBox checkPlanar;
+    JCheckBox checkDepth;
+    JCheckBox checkROI;
 
     Object[] options = {"Ok", "Help", "Cancel"};
 
-    @Override
+    public AnalyseListener(){
+        mainPanel = new JPanel(new GridLayout(3,2));
+        error = new JLabel("Allowed CS error (%)");
+        perError = new JTextField(5);
+        checkPlanar = new JCheckBox("Planar motion correction");
+        checkDepth = new JCheckBox("Depth motion correction");
+        checkROI = new JCheckBox("Find ROIs");
 
-    public void actionPerformed(ActionEvent e) {
         mainPanel.add(error);
         mainPanel.add(perError);
         mainPanel.add(checkPlanar);
         mainPanel.add(checkROI);
         mainPanel.add(checkDepth);
 
+        perError.setName("perError");
+        checkPlanar.setName("checkPlanar");
+        checkDepth.setName("checkDepth");
+        checkROI.setName("checkROI");
+    }
+
+    @Override
+
+    public void actionPerformed(ActionEvent e) {
+
         // Popup.
         JOptionPane.showOptionDialog(null, mainPanel, "Customise analysis",
                 JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null,
                 options, null);
+
+
+
     }
 }
--- src/test/java/TestHome.java
+++ src/test/java/TestHome.java
@@ -1,9 +1,7 @@
 import org.fest.swing.core.BasicRobot;
 import org.fest.swing.core.Robot;
 import org.fest.swing.finder.JFileChooserFinder;
-import org.fest.swing.fixture.FrameFixture;
-import org.fest.swing.fixture.JFileChooserFixture;
-import org.fest.swing.fixture.JMenuItemFixture;
+import org.fest.swing.fixture.*;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -42,6 +40,28 @@ public class TestHome {
         fileChooser.cancelButton().click();
         TimeUnit.SECONDS.sleep(1);
         controller.button("analyse").click();
+        TimeUnit.SECONDS.sleep(1);
+        controller.optionPane().textBox("perError").enterText("10");
+        TimeUnit.SECONDS.sleep(1);
+        controller.optionPane().checkBox("checkPlanar").click();
+        TimeUnit.SECONDS.sleep(1);
+        controller.optionPane().checkBox("checkDepth").click();
+        TimeUnit.SECONDS.sleep(1);
+        controller.optionPane().checkBox("checkROI").click();
+
+        TimeUnit.SECONDS.sleep(1);
+        controller.optionPane().buttonWithText("Ok").click();
+        TimeUnit.SECONDS.sleep(1);
+
+        controller.button("analyse").click();
+        TimeUnit.SECONDS.sleep(1);
+        controller.optionPane().buttonWithText("Help").click();
+        TimeUnit.SECONDS.sleep(1);
+
+        controller.button("analyse").click();
+        TimeUnit.SECONDS.sleep(1);
+        controller.optionPane().buttonWithText("Cancel").click();
+        TimeUnit.SECONDS.sleep(1);
 
     }
 }
